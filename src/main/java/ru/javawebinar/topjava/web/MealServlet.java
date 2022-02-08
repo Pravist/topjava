5f3d2574f344de808f6a4b5bd9ab997b758a742d
Pravist <pravist@mail.ru>
2022-02-08 10:51:56 +0500

    edit MealServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,12 +2,20 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
@@ -15,6 +23,20 @@ public class MealServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
-        response.sendRedirect("meals.jsp");
+        List<Meal> meals = MealsUtil.mealList;
+
+        final Map<LocalDate, Integer> caloriesSumByDate = new HashMap<>();
+        meals.forEach(meal -> caloriesSumByDate.merge(meal.getDate(), meal.getCalories(), Integer::sum));
+        final List<MealTo> mealsTo = new ArrayList<>();
+        meals.forEach(meal -> mealsTo.add(createWithExcess(meal, caloriesSumByDate.get(meal.getDate()) > MealsUtil.caloriesPerDay)));
+
+
+        request.setAttribute("mealsTo", mealsTo);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+
+    }
+
+    public static MealTo createWithExcess(Meal meal, boolean excess) {
+        return new MealTo(meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
     }
 }
